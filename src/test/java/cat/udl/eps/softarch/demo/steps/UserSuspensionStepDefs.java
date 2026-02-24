50a5aeb4f78a2d10c9978a2bde736cfe3f5a57ee
Miquel Baron <miquelbaron111@gmail.com>
2026-02-24 16:23:25 +0100

    Cucumber fixes

--- src/test/java/cat/udl/eps/softarch/demo/steps/UserSuspensionStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/UserSuspensionStepDefs.java
@@ -2,7 +2,7 @@ package cat.udl.eps.softarch.demo.steps;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import cat.udl.eps.softarch.demo.domain.Role;
 import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
 import io.cucumber.java.en.And;
@@ -17,6 +17,18 @@ public class UserSuspensionStepDefs {
 
     public UserSuspensionStepDefs(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+    @Given("There is a registered admin with username {string} and password {string} and email {string}")
+    public void thereIsARegisteredAdmin(String username, String password, String email) {
+        if (!userRepository.existsById(username)) {
+            User user = new User();
+            user.setId(username);
+            user.setEmail(email);
+            user.setPassword(password);
+            user.encodePassword();
+            user.setRole(Role.ADMIN);
+            userRepository.save(user);
+        }
     }
 
     @When("^I suspend the user \"([^\"]*)\"$")
