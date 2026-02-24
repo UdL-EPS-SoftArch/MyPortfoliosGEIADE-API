32f82a0daea7a6b5970d41fee2223281af90ae53
AlbertPerez7 <albertperez2004@gmail.com>
2026-02-24 16:12:18 +0100

    Update Project.java
    corregit project,java

--- src/main/java/cat/udl/eps/softarch/demo/domain/Project.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Project.java
@@ -2,16 +2,22 @@ package cat.udl.eps.softarch.demo.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.lang.Nullable;
 
 import java.time.ZonedDateTime;
 
 @Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Project extends UriEntity<Long> {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
     private String description;
@@ -27,23 +33,9 @@ public class Project extends UriEntity<Long> {
     //@ManyToOne
     //private Portfolio portfolio;
 
-    // Constructors, Getters i Setters
-    public Project() {
-        this.created = ZonedDateTime.now();
-    }
-
     @Nullable
     @Override
     public Long getId() {
         return this.id;
     }
-
-    public void setCreated(ZonedDateTime created) {
-        this.created = created;
-    }
-
-    public void setModified(ZonedDateTime modified) {
-        this.modified = modified;
-    }
-
 }
