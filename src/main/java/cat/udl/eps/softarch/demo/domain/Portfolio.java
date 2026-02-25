2e4a106585a4059316e08fabb294acd62f9b706b
Toni Cayuela Lopez <tonicayuelalopez@MacBook-Pro-de-Toni.local>
2026-02-25 17:32:15 +0100

    Fix: Delete the OneToMany

--- src/main/java/cat/udl/eps/softarch/demo/domain/Portfolio.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Portfolio.java
@@ -1,16 +1,12 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.time.ZonedDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
@@ -39,13 +35,6 @@ public class Portfolio extends UriEntity<Long> {
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
     private User owner;
-
-    // Portfolio contains many Projects
-    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnore
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    private Set<Project> projects = new HashSet<>();
 
     @PrePersist
     protected void onCreate() {
