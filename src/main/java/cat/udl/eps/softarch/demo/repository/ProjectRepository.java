8e4e04d903a83fa9ca151d3aaa7b90368ed7b57e
AlbertPerez7 <albertperez2004@gmail.com>
2026-04-16 14:16:48 +0200

    project changed

--- src/main/java/cat/udl/eps/softarch/demo/repository/ProjectRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ProjectRepository.java
@@ -1,7 +1,8 @@
 package cat.udl.eps.softarch.demo.repository;
 
+import cat.udl.eps.softarch.demo.domain.Portfolio;
 import cat.udl.eps.softarch.demo.domain.Project;
-import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.domain.Visibility;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +14,11 @@ import java.util.List;
 public interface ProjectRepository extends CrudRepository<Project, Long>, PagingAndSortingRepository<Project, Long> {
 
     List<Project> findByNameContaining(@Param("name") String name);
-    
+
+    List<Project> findByPortfolio(@Param("portfolio") Portfolio portfolio);
+
+    List<Project> findByPortfolioAndVisibility(@Param("portfolio") Portfolio portfolio,
+                                               @Param("visibility") Visibility visibility);
+
+    List<Project> findByVisibility(@Param("visibility") Visibility visibility);
 }
